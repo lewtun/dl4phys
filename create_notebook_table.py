fba806a1cfab476ef973c1400f7a33491150db53
lewtun <lewis.c.tunstall@gmail.com>
2022-05-13 11:53:38 +0200

    Lecture03 (#10)

--- create_notebook_table.py
+++ create_notebook_table.py
@@ -5,6 +5,7 @@ GITHUB_PATH_PREFIX = "lewtun/dl4phys/blob/main/"
 CHAPTER_TO_NB = {
     "1 - Jet tagging with neural networks": "lecture01",
     "2 - Gradient descent": "lecture02",
+    "3 - Neural network deep dive": "lecture03"
 }
 
 
